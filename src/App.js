8913f864a99fa5519bad2635d5f22c03ae18709e
Ulises Peña <marculises14@gmail.com>
2022-05-03 23:16:12 -0500

    1st commit v1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navegation } from "./components/Navegation";
+import { About } from "./components/sections/About";
+import { Home } from "./components/sections/Home";
+import { Team } from "./components/sections/Team";
+import { Faq } from "./components/sections/Faq";
+import { Footer } from "./components/Footer";
+
+import "./App.css";
+import "./styles/GlobalStyle.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="Theme">
+        <Navegation />
+        <Home />
+        <About />
+        <Team />
+        <Faq />
+        <Footer />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Navegation.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Navegation = () => {
+  return <div>Navegation</div>;
+};
