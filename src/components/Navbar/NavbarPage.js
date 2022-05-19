27b33b968efe2daa4cec07c62747cf57cfb525f0
Ulises Peña <marculises14@gmail.com>
2022-05-18 20:49:31 -0500

    Rediseño de página

--- src/components/Navbar/NavbarPage.js
+++ src/components/Navbar/NavbarPage.js
@@ -6,11 +6,20 @@ import "./NavbarPage.css";
 
 export const NavbarPage = () => {
   return (
-    <div className="section">
-      <nav class="navbar navbar-expand-lg navbar-dark ">
-        <div class="container-fluid ">
+    <div className="">
+      <nav className="navbar navbar-expand-lg navbar-dark section fixed-top">
+        <div className="container">
           <Link className="navbar-brand" to="/">
-            <h2>Clean Power</h2>
+            <h2
+              style={{
+                color: "#fff",
+                fontSize: "25px",
+                fontWeight: "700",
+                letterSpacing: "2px",
+              }}
+            >
+              ZEUZ
+            </h2>
           </Link>
           <button
             class="navbar-toggler"
@@ -21,10 +30,13 @@ export const NavbarPage = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse navBarU" id="navbarNavAltMarkup">
-            <div className="navbar-nav ">
+          <div
+            className="collapse navbar-collapse navBarU"
+            id="navbarNavAltMarkup"
+          >
+            <div className="navbar-nav ms-auto mb-2 mb-lg-0">
               <NavLink to="/Home">
                 <h5 className="nav-link" aria-current="page">
                   Home
@@ -41,7 +53,15 @@ export const NavbarPage = () => {
               </NavLink>
             </div>
             <button className="buttonContactU">
-              <h5>Contact</h5>
+              <h5
+                style={{
+                  fontSize: "15px",
+                  textTransform: "uppercase",
+                  marginTop: "2.2px",
+                }}
+              >
+                Contact
+              </h5>
             </button>
           </div>
         </div>
--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -1,26 +1,80 @@
 import React from "react";
-import { FormularioHome } from "../FormularioHome";
+import Elec1 from "../../assets/Elec1.jpg";
+import Elec2 from "../../assets/Elec2.jpg";
+import Elec3 from "../../assets/Elec3.jpg";
 
 import "./Home.css";
 
 export const Home = () => {
   return (
-    <div className="contenedor" id="row-home">
-      <div className="container ">
-        <div className="row align-items-center ">
-          <div className="col-lg-6 col-md-auto headerInfo">
-            <h1 className="welcome">Bienvenido a Clean Power</h1>
-            <br />
-            <h3 className="littleDesc">
-              En Clean Power creamos soluciones innovadoras
-            </h3>
-            <br />
+    <div
+      id="carouselExampleCaptions"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src={Elec1} className="d-block w-100" alt="Elec1" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>La solución que buscabas</h5>
+            <p>Some representative placeholder content for the second slide.</p>
           </div>
-          <div className="col-lg-6 col-md-auto formularioU">
-            <FormularioHome />
+        </div>
+        <div className="carousel-item">
+          <img src={Elec2} className="d-block w-100" alt="Elec2" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Siempre dedicados</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src={Elec3} className="d-block w-100" alt="Elec3" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Presencia internacional</h5>
+            <p>Some representative placeholder content for the third slide.</p>
           </div>
         </div>
       </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
     </div>
   );
 };
